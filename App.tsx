eed89dc7631475c56c86187dc0fa7a198a84ded7
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-03-17 20:32:41 -0500

    Borramos el screen 'solicitud prueba de manejo'

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Catalog from './src/components/Catalog'
-import CustomQuoteRequestScreen from './src/components/CustomQuoteRequestScreen'
+
 
 const App = () => {
   return (
-    <CustomQuoteRequestScreen/>
+    <Catalog />
   )
 }
 
