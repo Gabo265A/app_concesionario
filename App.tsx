d0b135310226490b4067cc30f8a91878ac70b104
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-03-17 21:01:12 -0500

    Desarrollo del screen 'Contacto'

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Catalog from './src/components/Catalog'
 import WorkshopServiceScreen from './src/components/WorkshopServiceScreen'
+import ContactScreen from './src/components/ContactScreen'
 
 
 const App = () => {
   return (
-    <WorkshopServiceScreen />
+    <ContactScreen />
   )
 }
 
