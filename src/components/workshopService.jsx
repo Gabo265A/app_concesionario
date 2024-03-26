9758dde38f434bf6b6b845729b733d12d7f84dee
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-03-26 18:41:58 -0500

    Mejora visual del screen 'Servicio de taller' usando las librerías de react native paper.

--- src/components/workshopService.jsx
+++ src/components/workshopService.jsx
@@ -1,51 +1,76 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { DatePickerInput, registerTranslation, es } from 'react-native-paper-dates';
+registerTranslation('es', es);
+import { TextInput, List, Button } from 'react-native-paper';
 
 const WorkshopServiceScreen = () => {
   const [date, setDate] = useState('');
-  const [serviceType, setServiceType] = useState('');
+  const [serviceType, setServiceType] = useState('Servicios');
   const [fullName, setFullName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [vehicleRegistrationPlate, setVehicleRegistrationPlate] = useState('');
 
-  
+  const [expanded, setExpanded] = useState(false); //Estado para ocultar el listado de servicios
+  const handlePress = () => setExpanded(!expanded); //Función para ocultar el listado de servicios
+
+  const serviceTypeList = [
+    { id: 1, name: 'Mantenimiento' },
+    { id: 2, name: 'Reparación' },
+    { id: 3, name: 'Revisión técnica' },
+    { id: 4, name: 'Otro...' }
+  ]; //Listado de servicios para la lista desplegable
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Fecha de Cita:</Text>
-      <TextInput
-        style={styles.input}
-        value={date}
-        onChangeText={setDate}
-        placeholder="Fecha de Cita"
-      />
-      
-      <Text style={styles.label}>Tipo de Servicio:</Text>
-      <TextInput
-        style={styles.input}
-        value={serviceType}
-        onChangeText={setServiceType}
-        placeholder="Tipo de Servicio (Mantenimiento, Reparación, Revisión Técnica)"
-      />
-      
-      <Text style={styles.label}>Nombre y Apellidos:</Text>
-      <TextInput
-        style={styles.input}
-        value={fullName}
-        onChangeText={setFullName}
-        placeholder="Nombre y Apellidos"
-      />
-      
-      <Text style={styles.label}>Número de Celular:</Text>
-      <TextInput
-        style={styles.input}
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
-        placeholder="Número de Celular"
-        keyboardType="phone-pad"
-      />
+    <ScrollView>
+      <View style={styles.container}>
+        <DatePickerInput
+          locale="es"
+          label="Seleccione la fecha de la cita"
+          value={date}
+          onChange={(d) => setDate(d)}
+          inputMode="start"
+          validRange={{ startDate: new Date(), endDate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 14) }}
+        />
 
-      <Button title="Agendar Cita"  />
-    </View>
+        <List.Section style={{ marginBottom: 23, marginTop: 23 }}>
+          <List.Accordion
+            title={serviceType}
+            left={props => <List.Icon {...props} icon="format-list-bulleted" />}
+            expanded={expanded}
+            onPress={handlePress}>
+            {serviceTypeList.map((item) => <List.Item key={item.id} title={item.name} onPress={() => { setServiceType(item.name); setExpanded(false); }} />)}
+          </List.Accordion>
+        </List.Section>
+
+        <TextInput
+          style={styles.input}
+          label="Nombres y apellidos"
+          value={fullName}
+          onChangeText={setFullName}
+          right={<TextInput.Icon icon="account" />}
+        />
+
+        <TextInput
+          style={styles.input}
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+          label="Número de celular"
+          keyboardType="numeric"
+          right={<TextInput.Icon icon="cellphone" />}
+        />
+
+        <TextInput
+          style={styles.input}
+          value={vehicleRegistrationPlate}
+          onChangeText={setVehicleRegistrationPlate}
+          label="Placa del vehículo"
+          right={<TextInput.Icon icon="car" />}
+        />
+
+        <Button icon="send" mode="contained">Solicitar servicio</Button>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -53,17 +78,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 25,
-  },
-  label: {
-    fontSize: 18,
-    marginBottom: 5,
+    backgroundColor: '#EEEEEE',
   },
   input: {
-    height: 45,
-    borderColor: 'red',
-    borderWidth: 1,
     marginBottom: 23,
-    paddingHorizontal: 12,
   },
 });
 
