93625d384552915e3b0f7c6204bd51b5ada839c1
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-04-19 17:57:30 -0500

    El catalogo se trae directamente desde la base de datos de firestore

--- src/components/VehicleList.jsx
+++ src/components/VehicleList.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
-import Vehicle from './Vehicle';
+import React, { useEffect } from 'react'
+import { ScrollView } from 'react-native'
+import Vehicle from './Vehicle'
+import firestore from '@react-native-firebase/firestore'
 
-const VehicleList = ({ vehicles }) => {
+const VehicleList = () => {
+    const [catalog, setCatalog] = React.useState([])
+
+    async function getCatalog() {
+        try {
+            const catalogCollection = await firestore().collection('catalog').get()
+            setCatalog(catalogCollection.docs.map((vehicle) => vehicle.data()))
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getCatalog()
+    }, [])
+
     return (
         <ScrollView>
-            {vehicles.map((vehicle) => (
+            {catalog.map((vehicle) => (
                 <Vehicle
                     key={vehicle.id}
-                    imageUrl={vehicle.imageUrl}
+                    imageUrl={vehicle.image}
                     description={vehicle.description}
-                    price={vehicle.price.valueOf()}
+                    price={Intl.NumberFormat('es-CO').format(vehicle.price)}
                     name={vehicle.name}
-                    iconCar={vehicle.iconCar}
+                    iconCar={vehicle.icon}
                 />
             ))}
         </ScrollView>
