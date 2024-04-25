aa420e3dce5b49fff930f521b4ecf145b13591cd
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-04-25 03:54:58 -0500

    Se agrego un indicador de carga en la screen. Solo aparece mientras se recupera la información de los vehículos desde la base de datos

--- src/screens/VehicleList.jsx
+++ src/screens/VehicleList.jsx
@@ -1,32 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import { ScrollView } from 'react-native'
+import React from 'react'
+import { ScrollView, View, StyleSheet } from 'react-native'
 import Vehicle from '../components/Vehicle'
-import { useVehicleContext, useGetCatalogList } from '../context/vehicles/vehicleContext'
+import { useVehicleContext, useIsLoadingData } from '../context/vehicles/vehicleContext'
+import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 
 const VehicleList = () => {
 
-    const getCatalog = useGetCatalogList()
-
-    useEffect(() => {
-        getCatalog()
-    }, [])
-
     const catalog = useVehicleContext()
+    const isLoadingData = useIsLoadingData()
 
     return (
-        <ScrollView>
-            {catalog.map((vehicle) => (
-                <Vehicle
-                    key={vehicle.id}
-                    imageUrl={vehicle.image}
-                    description={vehicle.description}
-                    price={Intl.NumberFormat('es-CO').format(vehicle.price)}
-                    name={vehicle.name}
-                    iconCar={vehicle.icon}
-                />
-            ))}
+        <ScrollView contentContainerStyle={isLoadingData && styles.centered}>
+            {isLoadingData ? (
+                <ActivityIndicator animating={true} color={MD2Colors.deepPurple500} size={100} />
+            ) : (
+                catalog.map((vehicle) => (
+                    <Vehicle
+                        key={vehicle.id}
+                        imageUrl={vehicle.image}
+                        description={vehicle.description}
+                        price={Intl.NumberFormat('es-CO').format(vehicle.price)}
+                        name={vehicle.name}
+                        iconCar={vehicle.icon}
+                    />
+                ))
+            )}
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    centered: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default VehicleList
