46ffa44b2490c6d80c294d1225534f09b8e2a8fa
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-04-25 16:40:43 -0500

    Se agrego una nueva función para buscar los vehículos que se mostrarán en la ventana de Buscar

--- src/context/vehicles/vehicleContext.jsx
+++ src/context/vehicles/vehicleContext.jsx
@@ -1,71 +1,82 @@
-import { useContext, useState, createContext } from "react"
+import { useContext, useState, createContext, useEffect } from "react"
 import firestore from '@react-native-firebase/firestore'
 
 const vehicleContext = createContext()
-const getCatalogList = createContext()
 const searchVehicle = createContext()
+const isLoadingData = createContext()
 
 export function useVehicleContext() {
     return useContext(vehicleContext)
-}
-
-export function useGetCatalogList() {
-    return useContext(getCatalogList)
 }
 
 export function useSearchVehicle() {
     return useContext(searchVehicle)
 }
 
+export function useIsLoadingData() {
+    return useContext(isLoadingData)
+}
+
 export function VehicleContext(props) {
 
     const [catalog, setCatalog] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
-    function searchVehicleFunction(name, brand, year, price) {
-        if (name === '' && brand === '' && year === 'Año' && price === '') {
-            return "No deje campos vacíos"
+    async function searchVehicleFunction(keyword, setVehicles, showActivity) {
+        showActivity(true)
+        await getCatalog()
+        const keywordArray = keyword.split(" ")
+        vehiclesReturn = []
+        catalog.filter(vehicle => {
+            const vehicleInformation = vehicle.brand + ";" + vehicle.model + ";" + vehicle.year + ";" + vehicle.price + "" + vehicle.name;
+            if (vehicleInformation.toLowerCase().includes(keyword.toLowerCase())) {
+                vehiclesReturn.push(vehicle)
+            } else {
+                for (let i = 0; i < keywordArray.length; i++) {
+                    if (vehicleInformation.toLowerCase().includes(keywordArray[i].toLowerCase())) {
+                        vehiclesReturn.push(vehicle)
+                        break;
+                    }
+                }
+            }
+        })
+        if (vehiclesReturn.length > 0) {
+            setVehicles(vehiclesReturn)
         } else {
-            let vehicles = catalog.filter(vehicle => {
-                if (vehicle.name.toLowerCase().includes(name.toLowerCase())) {
-                    vehicles.push(vehicle)
-                }
-                else if (vehicle.brand.toLowerCase().includes(brand.toLowerCase())) {
-                    vehicles.push(vehicle)
-                }
-                else if (vehicle.price - 20000000 <= price && vehicle.price + 20000000 >= price) {
-                    vehicles.push(vehicle)
-                }
-                else if (vehicle.year.includes(year)) {
-                    vehicles.push(vehicle)
-                }
-            })
-            return "vehicles"
+            setVehicles([])
         }
+        showActivity(false)
     }
 
     async function getCatalog() {
         try {
+            setIsLoading(true)
             const catalogCollection = await firestore().collection('catalog').get()
             setCatalog(catalogCollection.docs.map((doc) => {
                 const vehicle = doc.data();
                 vehicle.id = doc.id;
                 return vehicle;
             }))
-
+            setIsLoading(false)
         }
         catch (error) {
             console.log(error)
+            setIsLoading(false)
         }
     }
 
+    useEffect(() => {
+        getCatalog()
+    }, [])
+
     return (
-        <searchVehicle.Provider value={searchVehicleFunction}>
-            <getCatalogList.Provider value={getCatalog}>
+        <isLoadingData.Provider value={isLoading}>
+            <searchVehicle.Provider value={searchVehicleFunction}>
                 <vehicleContext.Provider value={catalog}>
                     {props.children}
                 </vehicleContext.Provider>
-            </getCatalogList.Provider >
-        </searchVehicle.Provider>
+            </searchVehicle.Provider>
+        </isLoadingData.Provider>
 
     );
 }
