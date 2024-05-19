ce90f166ac60b0b03e7b7c622a47adae22498494
Gabo265A <gabrielarboleda2000@hotmail.com>
2024-05-19 09:28:23 -0500

    Se agregaron estilos condicionales para el tamaño de la imagen cuando el menu desplegable este visible

--- src/components/BackgroundWelcomeScreen.jsx
+++ src/components/BackgroundWelcomeScreen.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import {useContext} from 'react';
 import {ImageBackground, View, StyleSheet} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
+import BottomBarContext from '../context/BottomBar/BottomBarContext';
 
 const BackgroundWelcomeScreen = ({children}) => {
+  const {isDrawerOpen} = useContext(BottomBarContext);
   return (
     <ScrollView>
       <ImageBackground
-        style={styles.image}
+        style={isDrawerOpen ? {...styles.image, height: 870} : styles.image}
         source={{
           uri: 'https://firebasestorage.googleapis.com/v0/b/appconcesionario-66cd6.appspot.com/o/sources%2Fwelcome-screen.jpg?alt=media&token=1b8e56df-28e2-4305-9c58-539d9065c1d6',
         }}
@@ -18,7 +20,7 @@ const BackgroundWelcomeScreen = ({children}) => {
 
 const styles = StyleSheet.create({
   image: {
-    height: 870,
+    height: 815,
   },
   container: {
     position: 'absolute',
