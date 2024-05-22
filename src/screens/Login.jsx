9ed866f81cfd08e5bab3c527537b193bc6e2531a
Gabo265A <gabrielarboleda2000@hotmail.com>
2024-05-22 08:14:32 -0500

    Login funcional

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,13 +1,158 @@
-import React, {useState} from 'react';
+import {useState, useContext, useRef, useEffect} from 'react';
 import {View, StyleSheet, Image} from 'react-native';
-import {Text, TextInput, Button, IconButton} from 'react-native-paper';
+import {
+  Text,
+  TextInput,
+  Button,
+  IconButton,
+  ActivityIndicator,
+  MD2Colors,
+} from 'react-native-paper';
 import BackgroundWelcomeScreen from '../components/BackgroundWelcomeScreen';
 import Icon from 'react-native-vector-icons/Ionicons';
+import UserContext from '../context/users/UserContext';
+import Helper from '../components/HelperText';
+import DialogAlert from '../components/DialogAlert';
+import ActiveContext from '../context/ActiveContext/ActiveContext';
 
 const Login = props => {
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState('gabriel@gmail.com');
+  const emailRef = useRef(null);
+  const [password, setPassword] = useState('Gabriel123"');
+  const passwordRef = useRef(null);
   const [showPassword, setShowPassword] = useState(false);
+  const {signIn, isLoading, didTryAutoLogin} = useContext(UserContext);
+  const {setActiveScreen} = useContext(ActiveContext);
+  //Estados para mostrar el helper
+  const [showHelper, setShowHelper] = useState(false);
+  const [customHelper, setCustomHelper] = useState([]);
+
+  //Estados para mostrar el dialog
+  const [visible, setVisible] = useState(false);
+  const [customHelperMessage, setCustomHelperMessage] = useState('');
+
+  useEffect(() => {
+    if (didTryAutoLogin) {
+      const timer = setTimeout(() => {
+        navigation.navigate('home');
+        setActiveScreen('home');
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [didTryAutoLogin]);
+
+  const validateEmail = email => {
+    const regex =
+      /^[^\s@]+@(gmail\.com|yahoo\.com|hotmail\.com|outlook\.com|aol\.com|protonmail\.com|zoho\.com|yandex\.com|mail\.com|icloud\.com)(\.mx|\.co|\.uk|\.net|\.org|\.gov|\.edu)?$/;
+    return regex.test(email);
+  };
+
+  const activeHelperTextInEmail = text => {
+    if (text.length === 0) {
+      setShowHelper(true);
+      setCustomHelper(['error', '¡No deje el campo del correo vacío!']);
+    } else if (!validateEmail(text)) {
+      setShowHelper(true);
+      setCustomHelper(['info', '¡Ingrese un correo válido!']);
+    } else {
+      setShowHelper(false);
+    }
+  };
+
+  const activeHelperTextInPassword = text => {
+    if (text.length === 0) {
+      setShowHelper(true);
+      setCustomHelper(['error', '¡No deje el campo de la contraseña vacío!']);
+    } else if (text.length <= 7) {
+      setShowHelper(true);
+      setCustomHelper([
+        'info',
+        '¡La contraseña debe tener al menos 8 caracteres!',
+      ]);
+    } else if (!validatePasswordUppercase(text)) {
+      setShowHelper(true);
+      setCustomHelper([
+        'info',
+        '¡La contraseña debe tener una letra mayúscula!',
+      ]);
+    } else if (!validatePasswordNumber(text)) {
+      setShowHelper(true);
+      setCustomHelper(['info', '¡La contraseña debe tener un número!']);
+    } else if (!validatePasswordSpecialCharacter(text)) {
+      setShowHelper(true);
+      setCustomHelper([
+        'info',
+        '¡La contraseña debe tener un carácter especial!',
+      ]);
+    } else if (validatePasswordSpaces(text)) {
+      setShowHelper(true);
+      setCustomHelper(['info', '¡La contraseña no debe tener espacios!']);
+    } else {
+      setShowHelper(false);
+    }
+  };
+
+  const validatePasswordSpaces = password => {
+    const regex = /\s/;
+    return regex.test(password);
+  };
+
+  const validatePasswordUppercase = password => {
+    const regex = /[A-Z]/;
+    return regex.test(password);
+  };
+
+  const validatePasswordNumber = password => {
+    const regex = /\d/;
+    return regex.test(password);
+  };
+
+  const validatePasswordSpecialCharacter = password => {
+    const regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    return regex.test(password);
+  };
+
+  const checkInformation = () => {
+    if (email === '' || password === '') {
+      setCustomHelperMessage('Todos los campos son obligatorios');
+      setVisible(true);
+    } else if (!validateEmail(email)) {
+      setCustomHelperMessage(
+        '¡Ingrese un correo válido!\n\nAlgunas veces los usuarios escriben sus correos y dejan espacios, recuerde que el correo no debe contener espacios.\n\nNota: Para evitar los bots en nuestros servicios, tomamos medidas para permitir el registro con algunos de los servicios de correo más usados. En caso de que su correo no sea soportado por nuestra aplicación, por favor contáctenos para ayudarle en su registro.',
+      );
+      setVisible(true);
+    } else if (password.length <= 7) {
+      setCustomHelperMessage(
+        '¡La contraseña debe tener al menos 8 caracteres!',
+      );
+      setVisible(true);
+    } else if (
+      !validatePasswordUppercase(password) ||
+      !validatePasswordNumber(password) ||
+      !validatePasswordSpecialCharacter(password) ||
+      validatePasswordSpaces(password)
+    ) {
+      setCustomHelperMessage(
+        '¡La contraseña debe tener al menos una letra mayúscula, un número, un carácter especial y no debe tener espacios!',
+      );
+      setVisible(true);
+    } else {
+      signIn(email, password)
+        .then(result => {
+          if (result[0] === 'wrongCredentials') {
+            setCustomHelperMessage(result[1]);
+            setVisible(true);
+          } else {
+            setCustomHelperMessage(result[1]);
+            setVisible(true);
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  };
 
   const navigation = props.navigation;
   return (
@@ -45,21 +190,85 @@ const Login = props => {
             </View>
             <View style={styles.dataContainer}>
               <TextInput
+                ref={emailRef}
                 placeholder="Correo eléctronico"
                 right={<TextInput.Icon icon="email" />}
+                onChangeText={text => {
+                  setEmail(text);
+                  activeHelperTextInEmail(text);
+                }}
+                value={email}
+                onBlur={() => {
+                  setShowHelper(false);
+                }}
+                onFocus={() => {
+                  activeHelperTextInEmail(email);
+                }}
+                keyboardType="email-address"
               />
-              <TextInput
-                placeholder="Contraseña"
-                secureTextEntry={!showPassword}
-                style={{marginTop: 10}}
-              />
-              <Icon
-                name={showPassword ? 'eye-off' : 'eye'}
-                size={24}
-                color="#4a4150"
-                onPress={() => setShowPassword(!showPassword)}
-                style={{position: 'absolute', right: 16, top: 80}}
-              />
+              {showHelper &&
+                emailRef.current &&
+                emailRef.current.isFocused() && (
+                  <Helper
+                    type={customHelper[0]}
+                    isVisible={showHelper}
+                    text={customHelper[1]}
+                  />
+                )}
+              <View
+                style={
+                  passwordRef.current &&
+                  passwordRef.current.isFocused() &&
+                  showHelper
+                    ? {
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingBottom: 23,
+                      }
+                    : {flexDirection: 'row', alignItems: 'center'}
+                }>
+                <TextInput
+                  ref={passwordRef}
+                  placeholder="Contraseña"
+                  secureTextEntry={!showPassword}
+                  style={{flex: 1, marginTop: 10, position: 'relative'}}
+                  value={password}
+                  onBlur={() => {
+                    setShowHelper(false);
+                  }}
+                  onFocus={() => {
+                    activeHelperTextInPassword(password);
+                  }}
+                  onChangeText={text => {
+                    setPassword(text);
+                    activeHelperTextInPassword(text);
+                  }}
+                />
+                <Icon
+                  name={showPassword ? 'eye-off' : 'eye'}
+                  size={24}
+                  color="#4a4150"
+                  onPress={() => setShowPassword(!showPassword)}
+                  style={{position: 'absolute', top: 26, right: 16}}
+                />
+                {showHelper &&
+                  passwordRef.current &&
+                  passwordRef.current.isFocused() && (
+                    <View
+                      style={{
+                        position: 'absolute',
+                        top: 65,
+                        right: 0,
+                        width: 340,
+                      }}>
+                      <Helper
+                        type={customHelper[0]}
+                        isVisible={showHelper}
+                        text={customHelper[1]}
+                      />
+                    </View>
+                  )}
+              </View>
               <Text style={{alignSelf: 'flex-end', paddingTop: 10}}>
                 ¿Olvidaste tu contraseña?{' '}
                 <Text
@@ -76,7 +285,7 @@ const Login = props => {
               <Button
                 icon="login"
                 mode="contained"
-                onPress={() => navigation.navigate('SignIn')}
+                onPress={() => checkInformation()}
                 style={{marginHorizontal: 10}}>
                 Iniciar sesión
               </Button>
@@ -92,6 +301,32 @@ const Login = props => {
           </View>
         </View>
       </BackgroundWelcomeScreen>
+      {visible && (
+        <DialogAlert
+          alertMessage={customHelperMessage}
+          changeVisibility={setVisible}
+        />
+      )}
+      {isLoading && (
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            pointerEvents: 'auto',
+          }}>
+          <ActivityIndicator
+            animating={true}
+            color={MD2Colors.deepPurple500}
+            size={100}
+          />
+        </View>
+      )}
     </>
   );
 };
