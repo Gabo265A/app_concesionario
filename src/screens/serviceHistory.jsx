13dfe0c98eb1d4d935a7a51a4b4f8b26f0679275
Gabo265A <gabrielarboleda2000@hotmail.com>
2024-05-23 15:42:41 -0500

    Se desarrollo toda la lógica para mostrar el historial de los usuarios logueados

--- src/screens/serviceHistory.jsx
+++ src/screens/serviceHistory.jsx
@@ -1,31 +1,84 @@
-import React, {useState} from 'react';
-import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
+import React, {useState, useContext, useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {List, MD3Colors} from 'react-native-paper';
+import {ScrollView} from 'react-native-gesture-handler';
+
+//Context
+import UserContext from '../context/users/UserContext';
+import ServiceHistoryContext from '../context/serviceHistory/ServiceHistoryContext';
+import {ca} from 'react-native-paper-dates';
 
 const ServiceHistoryScreen = () => {
   const [customerID, setCustomerID] = useState('');
-  const [serviceHistory, setServiceHistory] = useState([]);
+  const {userData} = useContext(UserContext);
+  const {serviceHistory, loadingHistory, getHistory} = useContext(
+    ServiceHistoryContext,
+  );
+  useEffect(() => {
+    if (userData) {
+      try {
+        getHistory(userData.uid);
+      } catch (error) {
+        console.log('Error al obtener el historial de servicios');
+      }
+    }
+  }, [userData]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>ID del Cliente:</Text>
-      <TextInput
-        style={styles.input}
-        value={customerID}
-        onChangeText={setCustomerID}
-        placeholder="ID del Cliente"
-        keyboardType="numeric"
-      />
+    <>
+      <View style={userData ? styles.container : styles.containerUserNotLogged}>
+        {userData ? (
+          <>
+            <ScrollView>
+              <Text style={{...styles.historyTitle, textAlign: 'center'}}>
+                Tu historial de servicios
+              </Text>
 
-      <Button title="Buscar Historial de Servicio" />
-
-      <Text style={styles.historyTitle}>Historial de Servicio:</Text>
-      {serviceHistory.map(item => (
-        <View key={item.id} style={styles.historyItem}>
-          <Text>Servicio: {item.serviceType}</Text>
-          <Text>Fecha: {item.date}</Text>
-        </View>
-      ))}
-    </View>
+              {serviceHistory ? (
+                <List.AccordionGroup>
+                  <View style={{paddingTop: 20}}>
+                    {serviceHistory.map((service, index) => (
+                      <List.Accordion
+                        key={index}
+                        title={service.serviceType}
+                        id={service.id}
+                        left={props => <List.Icon {...props} icon="history" />}>
+                        <List.Item title={'Fecha: ' + service.date} />
+                        <List.Item
+                          title={'Número de telefono: ' + service.phoneNumber}
+                        />
+                        <List.Item title={'Estado: ' + service.status} />
+                        <List.Item
+                          title={
+                            'Placa del vehículo: ' +
+                            service.vehicleRegistrationPlate
+                          }
+                        />
+                      </List.Accordion>
+                    ))}
+                  </View>
+                </List.AccordionGroup>
+              ) : (
+                <View style={{paddingTop: 20}}>
+                  <Text
+                    style={{
+                      ...styles.title,
+                      textAlign: 'center',
+                      fontSize: 15,
+                    }}>
+                    No tienes servicios en tu historial
+                  </Text>
+                </View>
+              )}
+            </ScrollView>
+          </>
+        ) : (
+          <Text style={{...styles.title, textAlign: 'center'}}>
+            Inicia sesión para ver el historial de servicio
+          </Text>
+        )}
+      </View>
+    </>
   );
 };
 
@@ -35,9 +88,21 @@ const styles = StyleSheet.create({
     padding: 25,
     backgroundColor: 'white',
   },
+  containerUserNotLogged: {
+    flex: 1,
+    padding: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
   label: {
     fontSize: 18,
     marginBottom: 5,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
   input: {
     height: 45,
